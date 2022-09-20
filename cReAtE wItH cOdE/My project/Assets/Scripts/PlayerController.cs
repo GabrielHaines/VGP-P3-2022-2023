4fac93f6006db678e4cf927893380e90ce2cc834
GabrielHaines <181591@student.vansd.org>
2022-09-20 10:00:11 -0700

    update 1.1.2
    -Removed old script
    -Added better vehicle moving script

--- cReAtE wItH cOdE/My project/Assets/Scripts/PlayerController.cs
+++ cReAtE wItH cOdE/My project/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //we will write the vehicle move script below this >:)
-        transform.Translate(0,0,1);
+        transform.Translate(Vector3.forward*Time.deltaTime* 20);
         
     }
 }
