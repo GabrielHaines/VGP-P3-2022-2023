b149ceb95344edf4b76f9acba2013a8d448d0d27
GabrielHaines <181591@student.vansd.org>
2022-09-22 10:41:44 -0700

    loolololol

--- cReAtE wItH cOdE/My project/Assets/Scripts/FollowPlayer.cs
+++ cReAtE wItH cOdE/My project/Assets/Scripts/FollowPlayer.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
     
-    
-    void Update()
+    void LateUpdate()
    {
-    transform.position = player.transform.position + new Vector3(0 , 5 , -7);
+    transform.position = player.transform.position + new Vector3(0 , 4 , -8);
    }
     // Offseted the camera behind the player^^^^^
   
