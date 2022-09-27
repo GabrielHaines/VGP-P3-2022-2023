eafcbb58a3812f5b5d25c09b26ccc091aa26dfdf
GabrielHaines <181591@student.vansd.org>
2022-09-27 10:29:46 -0700

    hipity hoppyity I stole your property

--- cReAtE wItH cOdE/My project/Assets/Scripts/PlayerController.cs
+++ cReAtE wItH cOdE/My project/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   private float speed = 5.0f;
-   private float turnspeed;
+   private float speed = 20.0f;
+   private float turnspeed = 25.0f;
    
 
    // Start is called before the first frame update
