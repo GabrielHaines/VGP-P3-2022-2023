b56ecbf543ded059135d422f1b780d82c0fdf9f1
GabrielHaines <181591@student.vansd.org>
2022-10-11 10:04:03 -0700

    dasda

--- cReAtE wItH cOdE/Prototype 2/Assets/scripts/PlayerController.cs
+++ cReAtE wItH cOdE/Prototype 2/Assets/scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   
    public float horizontalInput;
    public float speed = 10.0f;
        // Start is called before the first frame update
@@ -13,15 +14,23 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    
+   public float xRange = 10; 
     void Update()
     {
-      if (transform.position.x < -10)
+      if (transform.position.x < xRange)
       {
-       transform.position = new Vector3(-10,transform.position.y, transform.position.z);
+       transform.position = new Vector3(xRange,transform.position.y, transform.position.z);
       }
       
       horizontalInput = Input.GetAxis("Horizontal");
       transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+      
+       if (transform.position.x > xRange)
+      {
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+      }
+
     }
   
 }
