78f2678f60adae5247d9605ad9bdd146e319128d
GabrielHaines <181591@student.vansd.org>
2022-10-12 10:12:11 -0700

    Cooooooooooool

--- cReAtE wItH cOdE/Prototype 2/Assets/scripts/PlayerController.cs
+++ cReAtE wItH cOdE/Prototype 2/Assets/scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   
+   public GameObject projectilePrefab;
    public float horizontalInput;
    public float speed = 10.0f;
        // Start is called before the first frame update
@@ -30,7 +30,14 @@ public class PlayerController : MonoBehaviour
       {
         transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
       }
-
+      
+      if(Input.GetKeyDown(KeyCode.Space))
+      {
+       Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+      }    
+    
+    
+    
     }
   
 }
