29bd32875ad57ae552878d7f8115f940b7cbba64
GabrielHaines <181591@student.vansd.org>
2022-10-25 10:41:07 -0700

    dasdad

---
+++ cReAtE wItH cOdE/Prototype 2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x > topBound)
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+         else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
