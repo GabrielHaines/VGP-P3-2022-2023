23b0f8464a397b5ea1ae6fb27c7489591c609762
GabrielHaines <181591@student.vansd.org>
2022-10-31 09:46:45 -0700

    kllllllkpkjkuyghjkuhjkjkoiujkoiuhnkloiuhnkouyhjkouyghjiuyhjkuyghji87ythu87ytttt78o78888888888888888888888888888888888888888888888888888888888888888888878888888888888888888klllllllllllllhlkjhkljnhklhjklhjhkjhhsdjklsdfhkljfdjsdfjhdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjsodopifdjspfosifuaspodfiaspoifsuapfoiduaofusaodifuaspoifdusaoifsoifusdoapfsodsdfusfioudasdpfiousduioappsdfuioaasdpioiasdfuiaspuioasdfuioasdfioasdpfoiasdpfiosdfiouapasdfuiopasdfioupasdfiuosdoifasdopoiodfsaoasdifasdfiousdfuiosdfuiaoasdpfiisdpfuodasfuiosdfuioaaposfuoifadpoauisdfasdpfuioasdfiuoasdfiodfuioaspioasdfidfuosadasiofdiouasfpdiafdsiaidosfpoasiduioasdpfsdfoapiioduauioduiopuioioioiiiiiiiiiiiifiopiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- cReAtE wItH cOdE/Prototype 2/Assets/scripts/DestroyOutOfBoundsX.cs
+++ cReAtE wItH cOdE/Prototype 2/Assets/scripts/DestroyOutOfBoundsX.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float topBound = 30;
-    private float lowerBound = -10;
+    private float leftLimit = 30;
+    private float bottomLimit = -5;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > topBound)
+        if (transform.position.x > leftLimit)
         {
             Destroy(gameObject);
-        }  else if (transform.position.z < lowerBound)
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.z < bottomLimit)
         {
-        Debug.Log("Game Over");
-        Destroy(gameObject);
+            Destroy(gameObject);
         }
 
     }
--- cReAtE wItH cOdE/Prototype 2/Assets/scripts/DetectCollisionsX.cs
+++ cReAtE wItH cOdE/Prototype 2/Assets/scripts/DetectCollisionsX.cs
@@ -1,13 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollisionsX : MonoBehaviour
 {
 
-   private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
-        Destroy(other.gameObject);
     }
 }
